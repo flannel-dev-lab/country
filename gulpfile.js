72fdd3dbdabe44f91a17e2061d158fd0d41e14ba
Derek Sanford <derek@flanneldevlab.com>
2020-02-19 21:10:20 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var jsonlint = require('gulp-jsonlint');
+var log = require('fancy-log');
+
+gulp.src('./src/*.json')
+    .pipe(jsonlint())
+    .pipe(jsonlint.reporter(failureReporter));
+
+// Custom reporter for jsonlint to ensure Gulp exits correctly on fail for Travis tests.
+var failureReporter = function (file) {
+    log('File ' + file.path + ' is not valid JSON.');
+};
+
+gulp.task('ci', ['lint']);
